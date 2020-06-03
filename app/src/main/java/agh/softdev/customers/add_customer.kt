18edcfe019140c058cfc6d2377ca633ac82eaab6
Evel25 <bi3.h300@gmail.com>
2020-06-03 07:10:16 +0100

    creating all the activities needed for this app

---
+++ app/src/main/java/agh/softdev/customers/add_customer.kt
@@ -0,0 +1,12 @@
+package agh.softdev.customers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_customer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_customer)
+    }
+}
---
+++ app/src/main/java/agh/softdev/customers/customers.kt
@@ -0,0 +1,12 @@
+package agh.softdev.customers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class customers : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customers)
+    }
+}
